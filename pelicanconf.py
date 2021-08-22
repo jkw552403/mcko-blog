99e40c437dea52100098b2ce4a3d569ade7656ed
mcko <jkw552403@gmail.com>
2021-08-22 23:11:31 +0900

    support atom

--- pelicanconf.py
+++ pelicanconf.py
@@ -53,3 +53,8 @@ GOOGLE_ANALYTICS = 'UA-68128263-2'
 
 # Disqus
 DISQUS_SITENAME = 'mckos-blog'
+
+# Feed Items
+FEED_MAX_ITEMS = 15
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
